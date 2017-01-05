fc523783bd5c584734db709c8c4046967769b28a
Ben Giunta <bg9292@gmail.com>
2017-01-05 20:40:13 +0000

    added create contact action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105002420) do
+ActiveRecord::Schema.define(version: 20170105202629) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
