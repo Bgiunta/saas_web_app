574fc36ba91e5afd5923e212418b5b71ccf27076
Ben Giunta <bg9292@gmail.com>
2017-01-05 03:15:18 +0000

    added contact model file

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170105002420) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
-    t.string   "emal"
+    t.string   "email"
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
