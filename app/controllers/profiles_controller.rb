5830ff04d0958b4675e9fc19f80561fba3a96e73
Ben Giunta <bg9292@gmail.com>
2017-01-21 17:18:13 +0000

    Secured user pages with before filters

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
-  
+  before_action :authenticate_user!
+  before_action :only_current_user
   def new
     @profile = Profile.new
   end
@@ -29,12 +30,18 @@ class ProfilesController < ApplicationController
       if @profile.update_attributes(profile_params)
         flash[:success] = "Update Successful!"
         redirect_to user_path(id: params[:user_id])
+      else
+        render action: :edit
       end
-      
     end
     
     private
       def profile_params
         params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+      end
+      
+      def only_current_user
+        @user = User.find( params[:user_id] )
+        redirect_to(root_url) unless @user == current_user
       end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # GET to /users/:id
-  #
+  before_action :authenticate_user!
   def show
     @user = User.find( params[:id] )
   end
